d5e18d9bde17e93f03514f340decca92f046f9a1
Ben Hong <bencodezen@gmail.com>
2016-03-24 00:42:19 -0400

    Create first and simple React component

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+  StorePicker Component
+  This will let us make <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    return (
+      <p>hi</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.getElementById('main'));
