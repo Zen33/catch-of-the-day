50fdea600edc4366ec2f23bf9a5a69a381d0eb49
Ben Hong <bencodezen@gmail.com>
2016-04-11 15:31:49 -0400

    Add button to load data into state

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,11 @@ var App = React.createClass({
     // Set the state object
     this.setState({ fishes : this.state.fishes });
   },
+  loadSamples : function() {
+    this.setState({
+      fishes : require('./sample-fishes')
+    });
+  },
   render : function() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +40,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order/>
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
@@ -126,6 +131,7 @@ var Inventory = React.createClass({
         <h2>Inventory</h2>
 
         <AddFishForm {...this.props}/>
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
